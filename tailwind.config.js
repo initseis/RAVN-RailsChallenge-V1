73470b1fa0a08e8ed5451f3c65bc7365fbd4b501
Kevin <kevinsevinche@gmail.com>
2024-08-09 17:44:34 -0500

    Add colors for pokemon types

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'grey': '#F0F0F0'
-      },
-    },
+        'grey': '#F0F0F0',
+        'dark-grey': '#858585',
+        'fire': '#f08030',
+        'water': '#6890f0',
+        'grass': '#78c850',
+        'electric': '#f8d030'
+      }
+    }
   }
 }
