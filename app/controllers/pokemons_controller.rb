642c7b4a26d1ff5ec9b1f7bf866eeeb6def85dee
Kevin <kevinsevinche@gmail.com>
2024-08-09 17:45:52 -0500

    Add pokemon table and pokemon searcher

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,15 @@
 class PokemonsController < ApplicationController
   def index
+    @pokemons = current_user.pokemons.filterr(filter_params)
+  end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  private
+
+  def filter_params
+    params.permit(:name_or_type)
   end
 end
