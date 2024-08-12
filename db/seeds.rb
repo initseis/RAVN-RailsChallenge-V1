89089d37398bbfba3f011b897fbb7a228434a4fa
Kevin <kevinsevinche@gmail.com>
2024-08-12 10:52:05 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,13 @@ User.create!(first_name: 'Sam', last_name: 'Sepiol', email: 'sam@gmail.com', use
              password: 'k234234234', country: Country.first, role: :admin)
 User.create!(first_name: 'Ash', last_name: 'Ketchum', email: 'ash@gmail.com', password: 'k234234234',
              country: Country.first, role: :trainer)
+User.create!(first_name: 'Misty', last_name: 'Waterflower', email: 'misty@gmail.com', password: 'k234234234',
+             country: Country.second, role: :trainer)
+User.create!(first_name: 'Brock', last_name: 'Harrison', email: 'brock@gmail.com', password: 'k234234234',
+             country: Country.third, role: :trainer)
+Pokemon.create!(name: 'Pikachu', pokemon_type: :electric, main_technique: 'Thunderbolt',
+                description: 'A cute electric pokemon')
+Pokemon.create!(name: 'Charmander', pokemon_type: :fire, main_technique: 'Flamethrower',
+                description: 'A cute fire pokemon')
+Pokemon.create!(name: 'Squirtle', pokemon_type: :water, main_technique: 'Water Gun',
+                description: 'A cute water pokemon')
